9a6efb70ed441c205d928909343b872d52db8f83
Mukalele Rogers <mrogers4christ@gmail.com>
2018-07-21 13:09:08 +0300

    My Django Girls app, First Git Commit Evergit add --all

---
+++ python_intro.py
@@ -0,0 +1,17 @@
+
+#name = 'Mukalele'
+#if name == 'Mukalele':
+#	print('Mukalele Rogers is your name')
+#elif name == 'Rogers':
+#	print('Your could be another Rogers!')
+#else:
+#	print('Hello guest')
+
+def hi(name):
+	if name == 'hello':
+		print('Hello Django Girls!')
+		print('Great to catch up')
+	elif name == 'bye':
+		print('Good bye, Thanks of everything')
+
+hi('hello')
